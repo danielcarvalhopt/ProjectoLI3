0c4904e19fe6677fb0666f2076b668bff97e4022
Bruno Ferreira <chalkos@chalkos.net>
2012-03-11 20:11:34 +0000

    adicionei funções para lidar com strings, modifiquei os return values de algumas outras

--- utils.c
+++ utils.c
@@ -9,25 +9,6 @@ void clearInputBuffer(){
 	scanf("%*c");
 }
 
-char* readStr(char *ptr){
-	scanf("%500[^\n]", strBuffer); clearInputBuffer();
-	return allocStr(ptr, strBuffer);
-}
-
-char* allocStr(char *dest, char *src){
-	if( (dest = malloc(strlen(src) + 1)) == NULL ){
-		printf("Out of memory\n");
-		return NULL;
-	}
-	strcpy(dest, src);
-	return dest;
-}
-
-void readInt(int *ptr){
-	scanf("%d[^\n]",ptr);
-	clearInputBuffer();
-}
-
 void chooseSO(){
 	if( system("cls") ){
 		system("clear");
@@ -46,9 +27,75 @@ void clearScreen(){
 			system("cls");
 }
 
-void concatStr(char *dest, char *src){
-	dest = realloc( dest, (strlen(dest) + strlen(src) + 1) );
+int* readInt(int *ptr){
+	scanf("%d[^\n]",ptr);
+	clearInputBuffer();
+	return ptr;
+}
+
+char* readStr(char *ptr){
+	scanf("%500[^\n]", strBuffer); clearInputBuffer();
+	return allocStr(ptr, strBuffer);
+}
+
+char* allocStr(char *dest, char *src){
+	if( (dest = malloc(strlen(src) + 1)) == NULL ){
+		printf("Out of memory\n");
+		return NULL;
+	}
+	strcpy(dest, src);
+	return dest;
+}
+
+char* concatStr(char *dest, char *src){
+	if( (dest = realloc( dest, (strlen(dest) + strlen(src) + 1) )) == NULL ){
+		printf("Out of memory");
+		return NULL;
+	}
     strcat(dest,src);
+    return dest;
+}
+
+char* extendStr(char *str, int i){
+	if( (str = realloc( str, (strlen(str) + i + 1) )) == NULL ){
+		printf("Out of memory");
+		return NULL;
+	}
+	return str;
+}
+
+char* addCharEnd(char *dest, char *chr){
+	while(dest != '\0') dest++;
+	*(dest) = *chr;
+	*(dest+1) = '\0';
+	return (dest+1);
+}
+
+char* addnCharEnd(char *dest, char *chr, int i){
+	if( i > 0 ){
+		while(dest != '\0') dest++;
+		for(;i>0;i--){
+			*dest = *chr;
+			dest++;
+		}
+		*dest = '\0';
+	}
+	return dest;
+}
+
+char* addChar(char *dest, char *chr){
+	*(dest+1) = *chr;
+	*(dest+2) = '\0';
+	return (dest+2);
+}
+
+char* addnChar(char *dest, char *chr, int i){
+	for(;i>0;i--){
+		*dest = *chr;
+		dest++;
+	}
+	*dest = '\0';
+	return dest;
 }
 
 /*int main ()
--- utils.h
+++ utils.h
@@ -5,13 +5,17 @@ char strBuffer[501];
 char operativeSystem;
 
 void clearInputBuffer();
-char* readStr(char *ptr);
-char* allocStr(char *dest, char *src);
-void concatStr(char *dest, char *src);
-
-void readInt(int *ptr);
-
+int* readInt(int *ptr);
 void chooseSO();
 void clearScreen();
+
+char* readStr(char *ptr);
+char* allocStr(char *dest, char *src);
+char* concatStr(char *dest, char *src);
+char* extendStr(char *str, int i);
+char* addCharEnd(char *dest, char *chr);
+char* addnCharEnd(char *dest, char *chr, int i);
+char* addChar(char *dest, char *chr);
+char* addnChar(char *dest, char *chr, int i);
 
 #endif /* UTILS_H_INCLUDED */
